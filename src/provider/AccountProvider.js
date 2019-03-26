7faf61f4d91725135d7074ae48a0ccd036573b08
Harlan Evans <harlanevans5@gmail.com>
2019-03-26 14:01:23 -0600

    inti and final commit

---
+++ src/provider/AccountProvider.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const AccountContext = React.createContext();
+
+export const AccountConsumer = AccountContext.Consumer;
+
+class AccountProvider extends React.Component {
+  state = {
+    username: "harlotron",
+    dateJoined: "6/6/6",
+      membershipLevel: "Silver",
+    updateAccount: (account) => this.updateAccount(account),
+  };
+
+  updateAccount = (account) => {
+      this.setState({ ...account, });
+  }
+
+  render() {
+    return (
+      <AccountContext.Provider value={this.state}>
+        {this.props.children}
+      </AccountContext.Provider>
+    );
+  }
+}
+
+export default AccountProvider;
